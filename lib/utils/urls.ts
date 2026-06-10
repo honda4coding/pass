361774c96cadcbae283263eb19352cfdab688d43
honda4coding <mhndahmd820@gmail.com>
2026-06-10 17:40:19 +0300

    Fix: use env vars only for API and frontend URLs

--- lib/utils/urls.ts
+++ lib/utils/urls.ts
@@ -1,9 +1,3 @@
-export const isProduction = process.env.NODE_ENV === 'production'
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080/api/v1'
 
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || (isProduction
-    ? 'https://pass-backend-one.vercel.app/api/v1'
-    : 'http://localhost:8080/api/v1')
-
-export const FRONT_URL = process.env.NEXT_PUBLIC_FRONT_URL || (isProduction
-    ? 'https://pass-frontend.vercel.app'
-    : 'http://localhost:3000')
+export const FRONT_URL = process.env.NEXT_PUBLIC_FRONT_URL || 'http://localhost:3000'
